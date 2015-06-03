28a6e979fe9cf6cae3c84d619121db3117e3f683
Josh Goebel <dreamer3@gmail.com>
2015-06-02 22:30:28 -0400

    bug fixes

--- img2ard.rb
+++ img2ard.rb
@@ -47,7 +47,8 @@ class ImageCharArray
   end
 
   def to_s
-    header = "PROGMEM const unsigned char #{variable_name}[] = {\n"
+    header = "// #{File.basename(name)} / #{width}x#{height}\n"
+    header += "PROGMEM const unsigned char #{variable_name}[] = {\n"
     core = ""
     @data.each_with_index do |x, i|
       hex = x==0 ? "00" : x.to_s(16).upcase
@@ -66,23 +67,27 @@ resource = AssetFile.new("assets")
 files = Dir.glob("./assets/**/*.png")
 files.each do |file|
   img = ChunkyPNG::Image.from_file(file)
+  # puts img.inspect
   out = ImageCharArray.new(img, file)
   puts "#{file}: #{img.width}x#{img.height}"
 
   bits_last_page = img.height % 8
+  bytes_high = img.height / 8
+  bytes_high +=1 if bits_last_page>0
 
-  (0..img.width - 1).each do |x|
-    bytes_high = img.height / 8
-    (0..bytes_high - 1).each do |ypage|
+  (0..bytes_high - 1).each do |ypage|
+    (0..img.width - 1).each do |x|
       # how many bits does this line hold
       bits = 8
       # if we've reached the bottom there are fewer bits to load
-      bits = bits_last_page if bytes_high==ypage
+      bits = bits_last_page if bytes_high-1==ypage and bits_last_page > 0
       byte = 0
       (0..bits-1).each do |bit_height|
         px = img[x, ypage*8 + bit_height]
+        # print ChunkyPNG::Color.to_hex(px)
         # right now we only care about black/white so convert to greyscale
         c = ChunkyPNG::Color.grayscale_teint(px)
+        # puts("#{file} #{x}, #{ypage}, #{c} #{px}")
         if c > 0
           byte += (1 << (bit_height))
         end
@@ -90,6 +95,7 @@ files.each do |file|
       out.data << byte
     end
   end
+  # puts out.inspect
   resource.files << out
 end
 
